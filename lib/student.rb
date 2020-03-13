c16f881a232a7681ee61763428e9c68c0f2a656b
Kirsten Keels <keelsk@gmail.com>
2020-03-13 04:21:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,9 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    binding.pry
-    
-    
+    students_array.each {|data_hash| Student.new(data_hash)}
   end
 
   def add_student_attributes(attributes_hash)
